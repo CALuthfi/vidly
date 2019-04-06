dd7cfdd6967f5d37bf72c1f0619e596f892916c1
CALuthfi <choiriahmadluthfi@gmail.com>
2019-04-06 21:41:10 +0700

    Pagination-componentinterface

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const pagination = props => {
+  return null;
+};
+
+export default pagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
-
+import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4
   };
 
   handleDelete = movie => {
@@ -18,6 +19,10 @@ class Movies extends Component {
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    console.log(page);
   };
   render() {
     const { length: count } = this.state.movies;
@@ -63,6 +68,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={HTMLDListElement.handlePageChange}
+        />
       </React.Fragment>
     );
   }
